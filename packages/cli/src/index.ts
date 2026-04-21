4c03cc28feda7a20d82b2d17f576e872b08a9cb2
Gui Bibeau <GuiBibeau@gmail.com>
2026-04-21 15:31:30 -0400

    chore: bump mosaic packages to v0.1.2

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -19,7 +19,7 @@ import { controlCommand } from './commands/control/index';
 
 const program = new Command();
 
-program.name('mosaic').description('CLI for managing Token-2022 tokens with extensions').version('0.1.1');
+program.name('mosaic').description('CLI for managing Token-2022 tokens with extensions').version('0.1.2');
 
 // Create command group
 const createCommand = program.command('create').description('Create new tokens with Token-2022 extensions');
